0de48496144362850209ef5b8be9fa737163b8aa
Ilya Biin <i.biin@ostrovok.ru>
2013-08-23 19:44:07 +0400

    better goenv version

--- activate.sh
+++ activate.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-ENV_NAME=$1
-GO_VERSION=$2
-if [ -z "${ENV_NAME}" ]; then
-  echo "Goenv name is required"
-  return
-fi
-
-if [ -z "${GO_VERSION}" ]; then
-  GO_VERSION="1.1.1"
-  return
-fi
-
-
 platform='unknown'
 unamestr=`uname`
 MACH="amd64"
@@ -24,33 +11,64 @@ elif [[ "$unamestr" == 'Darwin' ]]; then
    platform='darwin'
 fi
 
-# DOWNLOADING GO section
-GO_URL="https://go.googlecode.com/files/go${GO_VERSION}.${platform}-${MACH}.tar.gz"
-GOENV_CORE="${HOME}/.goenv"
-GOENV_HOME="${HOME}/.goenv/${GO_VERSION}"
-mkdir -p $GOENV_CORE
-mkdir -p $GOENV_HOME
-GOENV_PATH="${GOENV_HOME}/${ENV_NAME}"
-mkdir -p ${GOENV_PATH}
-mkdir -p ${GOENV_PATH}/src
-mkdir -p ${GOENV_PATH}/pkg
-GO_DOWNLOAD_FILE="${GOENV_HOME}/go${GO_VERSION}.tar.gz"
-GO_DIR="${GOENV_HOME}/go-${GO_VERSION}"
-if [ ! -f $GO_DOWNLOAD_FILE ]; then
-	curl $GO_URL > $GO_DOWNLOAD_FILE
-fi
-if [ ! -d $GO_DIR ]; then
-	mkdir -p $GO_DIR
-	tar -xvzf $GO_DOWNLOAD_FILE -C $GO_DIR
-fi
+GO_VERSION=1.1.2
 
-# SETTING VARIABLES UP
-export GOROOT="${GO_DIR}/go"
-export GOBIN="${GOROOT}/bin"
-GO_IN_PATH=$(echo ":${PATH}:" | grep -q ":${GOBIN}:" | wc -l)
-if [[ $GO_IN_PATH -eq 0 ]]; then
-	export PATH="${GOBIN}:${PATH}"
-fi
+goenv_download() {  
+  # DOWNLOADING GO section
+  if [ ! -f $GO_DOWNLOAD_FILE ]; then
+  	curl $GO_URL > $GO_DOWNLOAD_FILE
+  	mkdir -p $GO_DIR
+  	tar -xvzf $GO_DOWNLOAD_FILE -C $GO_DIR
+  fi
+}
 
-export GOPATH="${GOENV_PATH}:$(pwd)"
+goenv_setup() {
+  # SETTING VARIABLES UP
+  GO_URL="https://go.googlecode.com/files/go${GO_VERSION}.${platform}-${MACH}.tar.gz"
+  GOENV_HOME="${HOME}/.goenv"
+  mkdir -p $GOENV_CORE
+  mkdir -p $GOENV_HOME
+  GOENV_PATH="${GOENV_HOME}/${GOENV_NAME}"
+  mkdir -p ${GOENV_PATH}
+  mkdir -p ${GOENV_PATH}/src
+  mkdir -p ${GOENV_PATH}/pkg
+  GO_DOWNLOAD_FILE="${GOENV_HOME}/go${GO_VERSION}.tar.gz"
+  GO_DIR="${GOENV_HOME}/go-${GO_VERSION}"
 
+  export GOROOT="${GO_DIR}/go"
+  export GOBIN="${GOROOT}/bin"
+  export GOPATH="${GOENV_PATH}:$(pwd)"
+
+  alias go="${GOROOT}/bin/go"
+  alias godoc="${GOROOT}/bin/godoc"
+  alias gofmt="${GOROOT}/bin/gofmt"
+}
+
+goclean() {
+  rm -rf ${GOENV_PATH}/pkg || true
+}
+
+gopurge() {
+  goclean
+  rm -rf ${GOENV_PATH} || true
+}
+
+goversion() {
+  if [[ ! -z $1 ]]; then
+    GO_VERSION=$1
+    goenv_setup
+    goenv_download
+  else
+    echo $GO_VERSION
+  fi
+  goclean
+}
+
+goon() {
+  GOENV_NAME=$(basename `pwd`)
+  if [[ ! -z $1 ]]; then
+    GOENV_NAME=$1
+  fi
+  goenv_setup
+  goenv_download
+}
