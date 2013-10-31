be7b8078c0aa4a5ff04aff8aae1c8dc5f3b055e6
Anton Egorov <anton.egoroff@gmail.com>
2013-10-31 09:15:54 +0400

    get gocode in the same manner as gom

--- activate.sh
+++ activate.sh
@@ -91,3 +91,14 @@ gom() {
   fi
   $GOBIN/gom $@
 }
+
+gocode() {
+  if [[ -z $GOENV_PATH ]]; then
+    echo "You should init goenv before using gocode"
+    return
+  fi
+  if [ ! -f $GOBIN/gocode ]; then
+    $GOBIN/go get github.com/nsf/gocode
+  fi
+  $GOBIN/gocode $@
+}
